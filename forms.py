c7a4fe164be11c7662733ca778d19ffe18fd0e57
Wudu-Molla <lawudula2@gmail.com>
2023-07-24 20:12:22 +0300

    initial commit

---
+++ forms.py
@@ -0,0 +1,49 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, SelectField
+from wtforms import URLField, EmailField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+
+
+class SignupForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
+    signup = SubmitField('Submit')
+
+
+class SignInForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    login = SubmitField('Submit')
+
+
+class AddForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    barcode = StringField('Barcode', validators=[DataRequired()])
+    price = IntegerField('Price', validators=[DataRequired()])
+    size = StringField('Size', validators=[DataRequired()])
+    image = URLField('Image URL', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    category = SelectField('Category', choices=('Shoes', 'Hats', 'Clothes'))
+    submit = SubmitField('submit')
+    #
+    # def validate_barcode(barcode):
+    #     product = Product.query.filter_by(barcode=barcode.data).first()
+    #     if product:
+    #         raise ValidationError('Barcode already exists!')
+
+
+class SearchForm(FlaskForm):
+    search = StringField("search", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class AddToCart(FlaskForm):
+    addToCart = SubmitField('Add')
+
+
+class RemoveFromCart(FlaskForm):
+    remove = SubmitField()
+
---
+++ shop.py
@@ -0,0 +1,207 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_required, UserMixin
+from flask_login import login_user, current_user, logout_user
+from forms import SignupForm, AddForm, SignInForm, AddToCart
+from forms import RemoveFromCart
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv('./.flaskenv')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+# 'postgresql://wudu:Wm867943322@localhost/shop'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.session_protection = "strong"
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(256), nullable=False)
+    barcode = db.Column(db.String(24), nullable=False, unique=True)
+    description = db.Column(db.String(1024))
+    price = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(512), nullable=False)
+    size = db.Column(db.String(48), nullable=False)
+
+    # category = db.Column(db.String(48), nullable=False)
+
+    def __repr__(self):
+        return f'id: {self.id}, title: {self.title}, barcode: {self.barcode}, description: {self.description}, ' \
+               f'Image: {self.image}, price: {self.price}, size: {self.size}'
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(256), nullable=False)
+    barcode = db.Column(db.String(24), nullable=False, unique=True)
+    description = db.Column(db.String(1024))
+    price = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(512), nullable=False)
+    size = db.Column(db.String(48), nullable=False)
+
+    # user_id = db.Column(db.Integer, db.ForeignKey('cart.id'),
+    #                     nullable=False)
+
+    def __repr__(self):
+        return f'id: {self.id}, title: {self.title}, barcode: {self.barcode}, description: {self.description}, ' \
+               f'Image: {self.image}, price: {self.price}, size: {self.size}'
+
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+
+    # cart = db.relationship('Cart', backref='cart', lazy=True)
+
+    def __repr__(self):
+        return f'id: {self.id}, email: {self.email}'
+
+
+#
+# @app.context_processor
+# def base():
+#     # form = SearchForm()
+#     form = AddForm()
+#     return dict(form=form)
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+@login_required
+def home():
+    form = AddToCart()
+    if form.validate_on_submit():
+        item_id = request.args.get('item')
+        items = Product.query.filter_by(id=item_id).first()
+        if items:
+            inCart = Cart.query.filter_by(barcode=items.barcode).first()
+            if inCart is None:
+                db.session.add(Cart(title=items.title,
+                                    barcode=items.barcode,
+                                    description=items.description,
+                                    price=items.price,
+                                    size=items.size,
+                                    image_url=items.image_url,
+                                    ))
+                db.session.commit()
+
+    data = Product.query.all()
+    return render_template('home.html', data=data, form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = SignInForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=True)
+                flash('Login successfully', category='success')
+                return redirect(url_for('home'))
+            else:
+                flash('Incorrect password' + user.email)
+        else:
+            flash('Invalid username', category='danger')
+            redirect(url_for('login'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    db.create_all()
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if not user:
+            if form.password.data == form.confirm_password.data:
+                password_hash: str = generate_password_hash(form.password.data)
+                db.session.add(Users(email=form.email.data, password=password_hash))
+                db.session.commit()
+                flash("Successfully created")
+            else:
+                flash('Passwords are not matching')
+        else:
+            flash('User already exists')
+    return render_template('signup.html', form=form)
+
+
+@app.route('/add_product/', methods=['GET', 'POST'])
+def add_product():
+    db.create_all()
+    form = AddForm()
+    if form.validate_on_submit():
+        db.session.add(Product(title=form.title.data,
+                               price=form.price.data,
+                               barcode=form.barcode.data,
+                               size=form.size.data,
+                               image_url=form.image.data,
+                               description=form.description.data))
+        db.session.commit()
+
+    form.title.data = ''
+    form.price.data = ''
+    form.barcode.data = ''
+    form.size.data = ''
+    form.image.data = ''
+    form.description.data = ''
+    flash('Added successfully')
+
+    return render_template('add_product.html', form=form)
+
+
+@app.route('/item', methods=['GET', 'POST'])
+def item():
+    barcode = request.args.get('search', '')
+    items = Product.query.filter_by(barcode=barcode).all()
+    item_by_id = Product.query.filter_by(id=request.args.get('item'))
+    return render_template('item.html', items=items, item=item_by_id)
+
+
+@app.route('/cart', methods=['GET', 'POST'])
+def cart():
+    form = RemoveFromCart()
+    if form.validate_on_submit():
+        remove_id = request.args.get('remove')
+        item_to_remove = Cart.query.get(remove_id)
+        db.session.delete(item_to_remove)
+        db.session.commit()
+    items = Cart.query.all()
+    return render_template('cart.html', items=items, form=form)
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.errorhandler(404)
+def error_404(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.config['DEBUG'] = 1
+    app.run()
