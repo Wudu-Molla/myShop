4e9073e32827c7ba4c873e85b4cd73286b9bf609
Wudu-Molla <lawudula2@gmail.com>
2023-07-25 16:19:42 +0300

    cleaning the code

--- shop/routes.py
+++ shop/routes.py
@@ -104,8 +104,8 @@ def add_product():
 def item():
     barcode = request.args.get('search', '')
     items = Product.query.filter_by(barcode=barcode).all()
-    cart_id = request.args.get('item')
-    item_by_id = Product.query.filter_by(id=cart_id)
+    item_id = request.args.get('item')
+    item_by_id = Product.query.filter_by(id=item_id).all()
     return render_template('item.html', items=items, cart_items=item_by_id)
 
 
